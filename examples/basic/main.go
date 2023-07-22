d9b6762691c43dbe04b6a1811d90e00d6305de62
sulaiman-coder <51925710+sulaiman-coder@users.noreply.github.com>
2023-07-22 16:28:20 +0600

    first commit

---
+++ examples/basic/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/khulnasoft-labs/goflags"
+)
+
+type Options struct {
+	name     string
+	Email    goflags.StringSlice
+	Phone    string
+	Address  goflags.StringSlice
+	fileSize goflags.Size
+	duration time.Duration
+}
+
+func main() {
+	testOptions := &Options{}
+	CheckUpdate := func() {
+		fmt.Println("checking if new version is available")
+		fmt.Println("updating tool....")
+	}
+
+	flagSet := goflags.NewFlagSet()
+	flagSet.CreateGroup("info", "Info",
+		flagSet.StringVarP(&testOptions.name, "name", "n", "", "name of the user"),
+		flagSet.StringSliceVarP(&testOptions.Email, "email", "e", nil, "email of the user", goflags.CommaSeparatedStringSliceOptions),
+	)
+	flagSet.CreateGroup("additional", "Additional",
+		flagSet.StringVarP(&testOptions.Phone, "phone", "ph", "", "phone of the user"),
+		flagSet.StringSliceVarP(&testOptions.Address, "address", "add", nil, "address of the user", goflags.StringSliceOptions),
+		flagSet.CallbackVarP(CheckUpdate, "update", "ut", "update this tool to latest version"),
+		flagSet.SizeVarP(&testOptions.fileSize, "max-size", "ms", "", "max file size"),
+		flagSet.DurationVar(&testOptions.duration, "timeout", time.Hour, "timeout"),
+	)
+
+	if err := flagSet.Parse(); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,28 @@
+module github.com/khulnasoft-labs/goflags
+
+go 1.20
+
+require (
+	github.com/cnf/structhash v0.0.0-20201127153200-e1b16c1ebc08
+	github.com/khulnasoft-labs/utils v0.0.45
+	github.com/pkg/errors v0.9.1
+	github.com/stretchr/testify v1.8.4
+	golang.org/x/exp v0.0.0-20230713183714-613f0c0eb8a1
+	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
+	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d // indirect
+	github.com/aymerick/douceur v0.2.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gorilla/css v1.0.0 // indirect
+	github.com/khulnasoft-labs/blackrock v0.0.1 // indirect
+	github.com/microcosm-cc/bluemonday v1.0.24 // indirect
+	github.com/miekg/dns v1.1.55 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
+	golang.org/x/mod v0.11.0 // indirect
+	golang.org/x/net v0.12.0 // indirect
+	golang.org/x/sys v0.10.0 // indirect
+	golang.org/x/tools v0.6.0 // indirect
+)
